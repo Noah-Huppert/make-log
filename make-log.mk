b0f7586ec9f651b6840430d65a81c682dc6773c6
Noah Huppert <noahhuppert@gmail.com>
2017-04-04 11:59:18 -0400

    Added tags

--- make-log.mk
+++ make-log.mk
@@ -1,3 +1,14 @@
+NO_COLOR=\033[0m
+
+OK_TAG=OK   #
+OK_COLOR=\033[32;01m
+
+WARN_TAG=WARN #
+WARN_COLOR=\033[33;01m
+
+ERROR_TAG=ERROR
+ERROR_COLOR=\033[31;01m
+
 define log = # level, message
 $(if $(findstring ok, $(1)), @printf "$(OK_COLOR)[$(OK_TAG)] $(2)$(NO_COLOR)\n")
 $(if $(findstring warn, $(1)), @printf "$(WARN_COLOR)[$(WARN_TAG)] $(2)$(NO_COLOR)\n")
